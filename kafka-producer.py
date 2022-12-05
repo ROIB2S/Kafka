07907b2564b39c942a1a4f872781f7d3be5e57fb
Grant Moyle <Grant.A.Moyle@roitraining.com>
2022-12-05 16:25:35 -0500

    Update kafka-producer.py

--- kafka-producer.py
+++ kafka-producer.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
 
+from kafka import KafkaProducer
+from time import sleep
+serverip = "10.128.11.##" # replace with your IP
+serverport = "9092"
+kafkaserver = serverip + ":" + serverport
+topicname = 'newtopic'
+messagecount = 100
+
+print("Connecting to Producer")
+producer = KafkaProducer(
+    bootstrap_servers=kafkaserver
+    )
+for counter in range(messagecount):
+    output = "Test Message " + str(counter+1) + " of " + str(messagecount)
+    print(output)
+    producer.send(
+        topicname, 
+        bytes(output,'utf-8')
+    )
+    sleep(1)
